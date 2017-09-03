0d78db2a0b972bddf962dd316378d05bc2548f5d
History <leftistlucy@hotmail.com>
2017-09-02 20:47:10 -0400

    First Commit

---
+++ CSharpTesting/FibonacciSequence.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace CSharpTesting
+{
+    public class FibonacciSequence
+    {
+        private List<uint> _testingValues;
+        private Dictionary<uint, BigInteger> _knownValues;
+        
+        public FibonacciSequence(uint value) {
+            // Initialize the collection of values to test.
+             this._testingValues = new List<uint>();
+
+            // Initiliaze the collection of known fibonacci values.
+            this._knownValues = new Dictionary<uint, BigInteger>();
+
+            do {
+                // Separate the value into separate steps.
+                this._testingValues.Add(value);
+                if (value > 1000) {
+                    value -= 1000;
+                }
+            } while (value > 1000);
+        }
+
+        public BigInteger NonRecCalculate() {
+            uint i = 1;
+
+            do {
+                ProcessFibonacci(i++);
+
+                if (i > 3) {
+                    this._knownValues.Remove(i - 3);
+                }
+            } while (i < this._testingValues[0]);
+
+            return ProcessFibonacci(this._testingValues[0]);
+        }
+
+
+        public BigInteger Calculate() {
+            // Loop through all the necessary steps to calculate the massive Fibonacci value.
+            for (int i = this._testingValues.Count - 1; i > 0; i--) {
+                // Calculate all the steps, and then remove them.
+                this.ProcessFibonacci(this._testingValues[i]);
+                this._testingValues.RemoveAt(i);
+
+                //TODO: Remove the unecessary dictionary values.
+            }
+
+            // Return the final value.
+            return ProcessFibonacci(this._testingValues[0]);
+        }
+
+
+        private BigInteger ProcessFibonacci(uint value) {
+            // Base cases.
+            if (value == 1 || value == 2) {
+                return 1;
+            } else {
+                // Have we already calulated the fibonacci value?
+                if (this._knownValues.ContainsKey(value)) {
+                    // We do. Return that instead of making a recursive call to calcluate it.
+                    var fibonacciValue = this._knownValues[value];
+
+                    // Remove the value from the dictionary. We no longer need it.
+                    this._knownValues.Remove(value);
+
+                    // Return the result.
+                    return fibonacciValue;
+                } else {
+                    // Add the calculated fibonacci value.
+                    this._knownValues.Add(value, ProcessFibonacci(value - 1) + ProcessFibonacci(value - 2));
+
+                    // Then return it.
+                    return this._knownValues[value];
+                }
+            }
+        }
+    }
+}
+
+/*
+ * 
+ * 
+ * 5 callbacks MAX
+ * 
+ * F5 -> 5 callbacks needed
+ * 
+ * F10 -> F9    -> F8   -> F7   -> F6   -> F5
+ * 
+ * 
+ * F10  -> F9   -> F8   -> F7   -> F6   -> F5   -> F4   -> F3   -> F2   -> F1
+ *                      -> F6
+ *              -> F7   -> F6
+ *                      -> F5
+ *              
+ *      -> F8   -> F7   -> F6
+ *                      -> F5
+ *              -> F6   -> F5
+ *                      -> F4
+ * 
+ * 
+ */
---
+++ CSharpTesting/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace CSharpTesting
+{
+    public class Program
+    {
+        private static void Main(string[] args) {
+
+            uint testingValue = 6900;
+
+
+            // Denote that we are starting the current test with the given value.
+            Console.WriteLine("Starting recursive optimized Fibonacci for {0}", testingValue);
+
+            // Store the current time.
+            var start = DateTime.Now;
+
+            var val = new FibonacciSequence(testingValue).Calculate();
+
+            // Subtract the starting time from the current time to get the elapsed time.
+            var end = DateTime.Now - start;
+
+            // Display results.
+            Console.WriteLine("Elapsed time: {0}ms", end);
+            Console.WriteLine("Val: {0:N0}", val);
+
+            // Spacing writeline.
+            Console.ReadKey();
+        }
+    }
+
+    public class FibonacciTest
+    {
+        private List<uint> _testingNumbers = new List<uint>();
+
+        // A private collection of known fibonaccis.
+        // I should have used a regular array, or a list, but using a dictionary was easier.
+        private Dictionary<int, BigInteger> _knownFibonaccis = new Dictionary<int, BigInteger>();
+
+        public FibonacciTest(uint number) {
+            // Keep on adding numbers to test.
+            // This is done to prevent a stackoverflowexception.
+            do {
+                this._testingNumbers.Add(number);
+                number = number / 2;
+            } while (number > 1000);
+        }
+
+        // UInt wasn't big enough, and only covers up to Fibonacci(92). Also, anything bigger
+        // than Fibonacci(6900) causes a StackOverflow.
+        public BigInteger OptimizedFibonacci(int value) {
+
+            // You can change the factor to remember a certain fibonacci value.
+            // E.G. if you've already calculated F(10), store the value so you don't have to 
+            // re-calculate it in the future.
+            // The tradeoff here is memory for time and processing.
+
+            // If you want to still use this approach, but use less memory and therefore take up more time and 
+            // processing, use a bigger factor number.
+            int factor = 1;
+
+            // Base case of F(1) and F(2)
+            if (value == 1 || value == 2) {
+                return 1;
+            } else {
+                // Do we have an already calculated fibonacci value?
+                if (_knownFibonaccis.ContainsKey(value)) {
+                    // We do. Return that instead of making a recursive call to calculate it.
+                    var val = _knownFibonaccis[value];
+                    _knownFibonaccis.Remove(value);
+                    return val;
+                } else {
+                    // Is the value we're asked to calculate a valid factor?
+                    if (value % factor == 0) {
+                        // Since it's a valid factor, and it's not contained in the dictionary, calculate it and store it.
+                        _knownFibonaccis.Add(value, OptimizedFibonacci(value - 1) + OptimizedFibonacci(value - 2));
+
+                        // Then return it.
+                        return _knownFibonaccis[value];
+                    }
+                }
+            }
+
+            // If everything else fails, just make the standard recursive call.
+            return OptimizedFibonacci(value - 1) + OptimizedFibonacci(value - 2);
+        }
+
+        public long RecursiveFibonaci(long value) {
+            // Base case.
+            if (value == 1 || value == 2) {
+                return 1;
+            } else {
+                // Recursive call.
+                return RecursiveFibonaci(value - 1) + RecursiveFibonaci(value - 2);
+            }
+        }
+    }
+
+    public class Sorter<T>
+    {
+        private T[] _data;
+        private Func<T, bool> _condition;
+
+        public Sorter(T[] data, Func<T, bool> condition) {
+            this._data = data;
+            this._condition = condition;
+        }
+
+        public T[] ListSort() {
+            Console.WriteLine("Starting {0} sorter...", "List");
+            var startTime = DateTime.Now;
+            
+            List<T> list = new List<T>();
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    list.Add(this._data[i]);
+                }
+            }
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            var array = list.ToArray();
+            return array;
+        }
+
+        public T[] O_2N_Sort() {
+
+            Console.WriteLine("Starting {0} sorter...", "O(2N)");
+            var startTime = DateTime.Now;
+            
+
+            int count = 0;
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    count++;
+                }
+            }
+
+            T[] sortedArray = new T[count];
+            count = -1;
+            for (int i = 0; i < this._data.Length && count < sortedArray.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    sortedArray[++count] = this._data[i];
+                }
+            }
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return sortedArray;
+        }
+
+        public T[] ArrayResizeSort() {
+            Console.WriteLine("Starting {0} sorter...", "ArrayResize");
+            var startTime = DateTime.Now;
+
+            T[] array = new T[0];
+
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    Array.Resize(ref array, array.Length + 1);
+                    array[array.Length - 1] = this._data[i];
+                }
+            }
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return array;
+        }
+
+        public T[] EnumerableSort() {
+            Console.WriteLine("Starting {0} sorter...", "Enumerable");
+            var startTime = DateTime.Now;
+
+
+            T[] array = YieldSort().ToArray();
+
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return array;
+        }
+
+        private IEnumerable<T> YieldSort() {
+            Console.WriteLine("Starting {0} sorter...", "Yield");
+            var startTime = DateTime.Now;
+
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    yield return this._data[i];
+                }
+            }
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+        }
+
+        public T[] LinqSort() {
+            Console.WriteLine("Starting {0} sorter...", "Linq");
+            var startTime = DateTime.Now;
+
+            var array = this._data.Where(this._condition).ToArray();
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return array;
+        }
+
+
+        public T[] LinkedListSort() {
+            Console.WriteLine("Starting {0} sorter...", "LinkedList");
+            var startTime = DateTime.Now;
+            var linkedList = new LinkedList<T>();
+
+
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+                    linkedList.AddLast(this._data[i]);
+                }
+            }
+
+            var array = linkedList.ToArray();
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return array;
+        }
+
+        public T[] CustomListSort() {
+            Console.WriteLine("Starting {0} sorter...", "CustomListSort");
+            var startTime = DateTime.Now;
+
+            T[] array = new T[0];
+            int arrayPtr = 0;
+
+            for (int i = 0; i < this._data.Length; i++) {
+                if (this._condition.Invoke(this._data[i])) {
+
+                    if (arrayPtr >= array.Length) {
+                        Array.Resize(ref array, (array.Length + 1) * 5);
+                    }
+
+                    array[arrayPtr++] = this._data[i];
+                }
+            }
+
+            Array.Resize(ref array, arrayPtr);
+
+            var elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("ElapsedTime: {0}", elapsedTime.ToString());
+            return array;
+        }
+    }
+}
